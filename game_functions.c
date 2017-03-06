8ce198e23b84733f96188f0fd931ea1260e975e1
Jason Andrews <jasonandrews271192@gmail.com>
2017-03-06 16:58:45 +0000

    Added game_functions.c and game_functions.h files to separate function prototypes and function implementation.

---
+++ game_functions.c
@@ -0,0 +1,13 @@
+/*
+ ============================================================================
+ Name        : gamefunctions.c
+ Authors     : 
+				Jason Andrews
+				Jeremiah Wangaruro
+				
+ Version     : 0.1
+ Description : 	This source file contains all the necessary code to 
+				implement the functions for the game.
+ ============================================================================
+*/
+
---
+++ game_functions.h
@@ -0,0 +1,13 @@
+/*
+ ============================================================================
+ Name        : gamefunctions.h
+ Authors     : 
+				Jason Andrews
+				Jeremiah Wangaruro
+				
+ Version     : 0.1
+ Description : 	This header file contains all the necessary function prototypes 
+				for implementing the functionality of the game.
+ ============================================================================
+*/
+
